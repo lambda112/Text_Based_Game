58e46bdd1b3a5d761298d8c3b5575a08552930d0
Kamran Azim <Kamazim121212@gmail.com>
2024-02-14 22:35:32 +0000

    Added more functionality and created an enemy class
    Added a method to calculate damage based on players highest attack stat (magic or strength). Also added a method to check if player is dead. Then created an enemy class which inherited from player class

--- main.py
+++ main.py
@@ -1,40 +1,58 @@
-class Characters:
-    
-    def __init__(self, name:str, health:int, strength:int, magic:int, attacks:dict):
+class Characters:   
+    def __init__(self, name:str, health:int, strength:int, magic:int, attacks:dict) -> None:
         self.name = name
         self.health = health
         self.strength = strength
         self.magic = magic
         self.attacks = attacks
-        
-    def calculate_level(self):
         power = self.strength + self.magic
         self.level = ((self.health * power) // 1000) * len(self.attacks)
-        return self.level
     
     def calculate_damage(self):
         for i in self.attacks:
-            self.attacks[i] += self.strength
+            if self.strength > self.magic:
+                self.attacks[i] += self.strength
+            else:
+                self.attacks[i] += self.magic
+
+
+    def is_dead(self):
+        if self.health > 0:
+            print(f"{self.name} has died.")
+            return True
+        else:
+            return False 
+
 
     def show_stats(self):
-        return f"Type: {self.name}, Health: {self.health}, Strength: {self.strength}, Magic: {self.magic}, Attacks: {self.attacks}, Level: {self.calculate_level()}"
+        return f"Type: {self.name}, Health: {self.health}, Strength: {self.strength}, Magic: {self.magic}, Attacks: {self.attacks}, Level: {self.level}"
+    
+
+class Enemy(Characters):
+    def __init__(self, name: str, health: int, strength: int, magic: int, attacks: dict) -> None:
+        super().__init__(name, health, strength, magic, attacks)
 
 
-warrior = Characters("Warrior", 100, 10, 0, {"Slash": 5, "Stab":3})
-mage = Characters("Mage", 100, 0, 10, {"Fireball":5, "Zap":3})
-archer = Characters("Archer", 100, 5, 5, {"Multi-Shot": 5, "Single-Shot":3})
+
+warrior_char = Characters("Warrior", 100, 10, 0, {"Slash": 5, "Stab":3})
+mage_char = Characters("Mage", 100, 0, 10, {"Fireball":5, "Zap":3})
+archer_char = Characters("Archer", 100, 5, 5, {"Multi-Shot": 5, "Single-Shot":3})
+spider_enemy = Enemy("Spider", 100, 5, 5, {"Web-Shot": 5, "Poison-Bite": 3})
 
 
 def display_stats():
-    print(warrior.show_stats())
-    print(mage.show_stats())
-    print(archer.show_stats())
+    print(warrior_char.show_stats())
+    print(mage_char.show_stats())
+    print(archer_char.show_stats())
 
 
-def choose_char():
-    character = {warrior.name:warrior, mage.name: mage, archer.name: archer}
+def choose_char(show_stats: bool = False, name: str = "Warrior",):
+    character = {"Warrior": warrior_char, "Mage": mage_char, "Archer": archer_char}
+    
+    if show_stats:
+        return character[name].show_stats()
+    
     user_char = input("Choose a character: ").capitalize()
-
     while user_char not in character.keys():
         user_char = input("Choose a valid character: ").capitalize()
 
@@ -44,3 +62,9 @@ def choose_char():
 
     return user_char
 
+
+char = choose_char()
+char.calculate_damage()
+spider_enemy.calculate_damage()
+
+print(choose_char(True, char.name))
